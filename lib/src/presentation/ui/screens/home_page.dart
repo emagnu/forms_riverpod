a2e2e9fc1eaf92d5a82a91a4341bceff5f9ecebe
Emmanuele <emagnubcp@gmail.com>
2024-05-23 18:40:24 +0100

    add FilledButton() and a GlobalKey

--- lib/src/presentation/ui/screens/home_page.dart
+++ lib/src/presentation/ui/screens/home_page.dart
@@ -1,23 +1,21 @@
 //  //   ///
 //  Import LIBRARIES
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
 //  Import FILES
 import '../../widgets/todo_list.dart';
 //  //   ///
 
-class HomePage extends ConsumerStatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override
-  ConsumerState<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends ConsumerState<HomePage> {
-  String _email = '';
+  // final String _email = '';
 
   @override
   Widget build(BuildContext context) {
+    // final TextEditingController emailController = TextEditingController();
+    GlobalKey formGlobalKey = GlobalKey<FormState>();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Todo List'),
@@ -33,18 +31,43 @@ class _HomePageState extends ConsumerState<HomePage> {
             const Expanded(child: TodoList()),
 
             //  Forms for adding new tasks - FormField()
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(label: Text('Email Address')),
-              onChanged: (value) {
-                setState(() {
-                  _email = value;
-                  debugPrint(value);
-                });
-              },
+            // TextField(
+            //   controller: emailController,
+            //   keyboardType: TextInputType.emailAddress,
+            //   decoration: const InputDecoration(label: Text('Email Address')),
+            //   // onChanged: (value) {setState(() {_email = value;debugPrint(value);});},
+            // ),
+            Form(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  // decoration: const InputDecoration(labelText: 'New Task',),
+                  // Todo title
+
+                  // Tod description
+
+                  // Priority
+
+                  // Submit button
+                  const SizedBox(height: 16.0),
+                  FilledButton(
+                    key: formGlobalKey,
+                    onPressed: () {},
+                    style: FilledButton.styleFrom(
+                      backgroundColor: Colors.grey[800],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4.0),
+                      ),
+                    ),
+                    child: const Text('Add todo'),
+                  ),
+                ],
+              ),
             ),
             const SizedBox(height: 16.0),
-            Text('Your email: $_email'),
+            // Text('Your email: $_email'),
+            // FilledButton(onPressed: () {debugPrint('EmailController: $emailController.text.trim()');},child: const Text('Print email'),),
+
             const SizedBox(height: 16.0),
           ],
         ),
